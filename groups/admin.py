dbbde9388aad141881f8e6024d3431009307d7e3
pritishgupta <pritish9040246119@gmail.com>
2018-05-15 09:16:49 +0530

    first commit

---
+++ groups/admin.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+
+# Register your models here.
+from . import models
+class GroupMemberInline(admin.TabularInline):
+    model=models.GroupMember
+admin.site.register(models.Group)
